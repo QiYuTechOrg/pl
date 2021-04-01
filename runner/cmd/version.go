95a66b317e7ddba4eae46312acf4cc99a280efba
dev <dev@qiyutech.tech>
2021-04-01 16:50:42 +0800

    add version cmd

---
+++ runner/cmd/version.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+    "github.com/spf13/cobra"
+)
+
+func init() {
+    rootCmd.AddCommand(version)
+}
+
+var version = &cobra.Command{
+    Use:   "version",
+    Short: "显示当前版本",
+    Long:  "显示软件的当前版本",
+    Run: func(cmd *cobra.Command, args []string) {
+        println("v0.0.6")
+    },
+}
